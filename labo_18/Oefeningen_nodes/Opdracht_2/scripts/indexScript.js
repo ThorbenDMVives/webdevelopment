fa3226d06329ef3a3809cdd93a8c6232fc1be502
ThorbenDMVives <thorben.demeulemeester@student.vives.be>
2026-03-31 22:38:30 +0200

    labo_19

--- labo_18/Oefeningen_nodes/Opdracht_2/scripts/indexScript.js
+++ labo_18/Oefeningen_nodes/Opdracht_2/scripts/indexScript.js
@@ -5,7 +5,7 @@ const setup = () => {
 
     const img = document.createElement("img")
     const body = document.querySelector("body");
-    img.setAttribute("src", "../Images/punten.jpg")
+    img.setAttribute("src", "../images/punten.jpg")
     img.setAttribute("alt", "photo")
     body.appendChild(img);
     console.log(img.src);
